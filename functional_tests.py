c67b4a77deae8f4bf9be486fafdfd10e07b49c93
Lww0611 <161685777+Lww0611@users.noreply.github.com>
2025-05-10 21:24:01 +0800

    message 8：Before refactoring the functional_tests"

--- functional_tests.py
+++ functional_tests.py
@@ -26,10 +26,18 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)  # 等待页面更新
 
-        # 验证任务是否显示在列表中
+        # 页面中又显示了一个文本输入框，可以输入其他待办事项
+        # 他输入了"Give a gift to Lisi"
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Give a gift to Lisi')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # 页面再次更新，她的清单中显示了这两个待办事项
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Buy flowers', [row.text for row in rows])
+        self.assertEqual('1: Buy flowers', [row.text for row in rows])
+        self.assertEqual('2: Give a gift to Lisi', [row.text for row in rows])
 
         # 测试未完成标记
         self.fail('Finish the test!')
